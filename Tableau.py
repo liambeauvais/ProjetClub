899a3137b00e5e40e65ead88c0f09d6cb1cd448a
Liam Beauvais <89455032+liambeauvais@users.noreply.github.com>
2022-01-25 14:11:48 +0100

    projet fonctionnel, voit TODOLIST

--- Tableau.py
+++ Tableau.py
@@ -2,28 +2,28 @@
 
 
 class Joueur:
-    def __init__(couille, name, licence, points):
-        couille.name = name
-        couille.licence = licence
-        couille.points = points
-        couille.player = [couille.name, couille.licence, couille.points]
+    def __init__(self, name, licence, points):
+        self.name = name
+        self.licence = licence
+        self.points = points
+        self.player = [self.name, self.licence, self.points]
 
 
 class Tableau:
-    def __init__(couille, nomFichier):
-        couille.nomFichier = nomFichier
+    def __init__(self, nomFichier):
+        self.nomFichier = nomFichier
         file = open(nomFichier)
         data = file.read()
-        couille.tableau = Tableau.get_tableau(couille, data)
+        self.tableau = Tableau.get_tableau(self, data)
 
-    def get_tableau(couille, data):
+    def get_tableau(self, data):
         tableau = data.split('\n')
         matrice = []
         for infos in tableau:
             infosJoueur = infos[1:len(infos) - 1]
             tabInfos = infosJoueur.split(',')
             print(tabInfos)
-            if couille.nomFichier == 'data.txt':
+            if self.nomFichier == 'data.txt':
                 objetJoueur = Joueur(tabInfos[0].replace("\'", ""),
                                      tabInfos[1].replace("\'", "").lstrip(),
                                      tabInfos[2].replace("\'", "").lstrip())
@@ -36,36 +36,36 @@ class Tableau:
                 matrice.append(objetRencontre.rencontre)
         return matrice
 
-    def afficher(couille):
-        for infos in couille.tableau:
+    def afficher(self):
+        for infos in self.tableau:
             print(infos)
 
-    def modifierData(couille):
+    def modifierData(self):
         stringFile = ""
-        file = open(couille.nomFichier, "w")
-        for i in range(len(couille.tableau) - 1):
-            string = str(couille.tableau[i]) + "\n"
+        file = open(self.nomFichier, "w")
+        for i in range(len(self.tableau) - 1):
+            string = str(self.tableau[i]) + "\n"
             stringFile += string
-        stringFile += str(couille.tableau[len(couille.tableau) - 1])
+        stringFile += str(self.tableau[len(self.tableau) - 1])
         file.write(stringFile)
         file.close()
 
-    def ajouterJoueur(couille):
+    def ajouterJoueur(self):
         nom = input("Nom du joueur?")
         prenom = input("prenom du joueur?")
         licence = input("n° de licence?")
         points = input("points officiels?")
         player = Joueur(nom + ' ' + prenom, str(licence), str(points))
-        couille.tableau.append(player.player)
+        self.tableau.append(player.player)
 
-    def triRencontre(couille,joueur:str) -> list:
+    def triRencontre(self, joueur:str) -> list:
         """
-        :param joueur: blzlblzlkzeljflize
+        :param joueur: string
         :type joueur: str
         :return: ca retourne une liste des rencontres du joueur demande
         """
         rencontres_du_joueur = []
-        for item in couille.tableau:
+        for item in self.tableau:
             if item[2] == joueur:
                 rencontres_du_joueur.append(item)
         a_joue_dans_equipe = []
